e059ff533d2574343569d956769b5eb9593dff3d
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-03-07 22:11:17 +0800

    Implemented rough version of heapsort

--- Heap/MaxHeapArr.c
+++ Heap/MaxHeapArr.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define MAX 15
+#define MAX 6
 
 typedef struct {
     int arr[MAX];
@@ -13,19 +13,22 @@ typedef struct {
 int parent(int);
 int left(int);
 int right(int);
-int maxHeapify(HeapPtr);
+void maxHeapify(HeapPtr, int);
 int buildMaxHeap(HeapPtr);
 void initHeap(HeapPtr);
 void printArr(HeapPtr);
+void heapSort(HeapPtr);
 
 int main() {
     Heap H;
-    int index = 2;
+    int index = 0;
     initHeap(&H);
     printArr(&H);
-    printf("Node: %d\n", H.arr[index]);
-    printf("Left: %d\n", H.arr[left(index)]);
-    printf("Right: %d\n", H.arr[right(index)]);
+    printf("\n");
+    // buildMaxHeap(&H);
+    // maxHeapify(&H, index);
+    heapSort(&H);
+    printArr(&H);
     return 0;
 }
 
@@ -36,8 +39,8 @@ void initHeap(HeapPtr HP) {
         ctr++;
     }
 
-    HP->heapSize = 0;
-    HP->length = 0;
+    HP->heapSize = ctr;
+    HP->length = ctr;
 }
 
 int parent(int index) {
@@ -50,6 +53,75 @@ int left(int index) {
 
 int right(int index) {
     return 2*(index+1);
+}
+
+void maxHeapify(HeapPtr HP, int index) {
+    // Maintains heap property
+
+    // Get indices for left and right child of current index
+    int l = left(index);
+    int r = right(index);
+
+    // Index to store the largest value among the node and its children
+    int largest;
+
+    // Check if curent node is greater than its left child
+    // Use l <= HP->heapSize so that no element that's part of the heap is checked
+    // which also prevents using indices that are greater than the array's length
+    if (l <= HP->heapSize && HP->arr[l] > HP->arr[index]) largest = l;
+    else largest = index;
+
+    // Check if right child is greater than the greatest value between
+    // the current node and its left child
+    if (r <= HP->heapSize && HP->arr[r] > HP->arr[index]) largest = r;
+
+    // If current node doesn't have the largest value, swap it out w/ the node
+    // that has the largest value and repeat procedure on that node
+    if (largest != index) {
+        int temp = HP->arr[largest];
+        HP->arr[largest] = HP->arr[index];
+        HP->arr[index] = temp;
+
+        maxHeapify(HP, largest);
+    }
+}
+
+int buildMaxHeap(HeapPtr HP) {
+    // Set heap size to match the length of the input array
+    HP->heapSize = HP->length;
+
+    // Make index start at the halfpoint of the array
+    // since the second half of the array = leaves
+    // and so are already max-heaps
+    int index = floor(HP->length / 2) - 1;
+    while (index >= 0) {
+        maxHeapify(HP, index);
+        index--;
+    }
+}
+
+void heapSort(HeapPtr HP) {
+
+    // Build a max heap from the array
+    buildMaxHeap(HP);
+
+    // Start from the last node in the heap
+    int ctr = HP->length - 1;
+    while (ctr > 0) {
+
+        // Swapm out root with last node
+        int temp = HP->arr[ctr];
+        HP->arr[ctr] = HP->arr[0];
+        HP->arr[0] = temp;
+
+        // Disregard last node of the heap
+        HP->heapSize--;
+
+        // Re-establish max heap property w/ node
+        // since swapping it out may have violated it
+        maxHeapify(HP, 0);
+        ctr--;
+    }
 }
 
 void printArr(HeapPtr HP) {
